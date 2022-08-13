526aa90b8f8f52f671982fea47ec4e2526c84309
lucaargolo <lucaargolo@gmail.com>
2022-08-12 21:24:27 -0300

    devenv goes brrrrrr

---
+++ src/main/kotlin/io/github/lucaargolo/craftingbench/CraftingBench.kt
@@ -0,0 +1,13 @@
+package io.github.lucaargolo.craftingbench
+
+import net.fabricmc.api.ModInitializer
+
+object CraftingBench: ModInitializer {
+
+    const val MOD_ID = "craftingbench"
+
+    override fun onInitialize() {
+
+    }
+
+}
---
+++ src/main/kotlin/io/github/lucaargolo/craftingbench/client/CraftingBenchClient.kt
@@ -0,0 +1,11 @@
+package io.github.lucaargolo.craftingbench.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object CraftingBenchClient: ClientModInitializer {
+
+    override fun onInitializeClient() {
+
+    }
+
+}
